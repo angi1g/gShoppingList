1102ca1785711bc6d18572e54d2ef47d226fbc01
Giacomo Angione <angi1g@gmail.com>
2024-02-26 11:32:49 +0100

    Initial Commit

---
+++ gShoppingList/gShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  gShoppingListApp.swift
+//  gShoppingList
+//
+//  Created by Barbara on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct gShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
